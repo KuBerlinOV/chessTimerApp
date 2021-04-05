94818ee730b48f208b5b0f6e83a3ba4cc20284df
KuBerlinOV <bkuberlinov@gmail.com>
2021-04-05 17:42:51 +0200

    added some css

--- main.js
+++ main.js
@@ -4,71 +4,91 @@ const timeWhite = document.querySelector('.time-w');
 const plwBtn = document.querySelector('.plw-btn')
 let timeSetWhite = 180;
 
+let intervalW = null;
+
 //blacks
 
 const timeBlack = document.querySelector('.time-b');
 const plbBtn = document.querySelector('.plb-btn')
 let timeSetBlack = 180;
 
-//select timer
+let intervalB = null;
 
+//shared
+const resetBtn = document.querySelector('.reset-btn')
 const gameSelector = document.querySelector('#gm-selector')
+let count = false;
+let gameType = 'bullet';
+let isGameOver = false;
 
-//set timer
+//choose gametype
 
 gameSelector.addEventListener('change', (event) => {
-    let gameType = event.target.value;
+    gameType = event.target.value;
     setGameType(gameType)
 })
-
 const setGameType = (type) => {
     switch (type) {
         case 'bullet':
             timeSetWhite = 180
             timeSetBlack = 180
+            timeWhite.innerHTML = '03:00';
+            timeBlack.innerHTML = '03:00';
             break;
         case 'blitz':
             timeSetWhite = 480
             timeSetBlack = 480
+            timeWhite.innerHTML = '08:00';
+            timeBlack.innerHTML = '08:00';
             break;
         case 'rapid':
             timeSetWhite = 1500
             timeSetBlack = 1500
+            timeWhite.innerHTML = '25:00';
+            timeBlack.innerHTML = '25:00';
+            break;
+        case 'armageddon':
+            timeSetWhite = 1800
+            timeSetBlack = 2100
+            timeWhite.innerHTML = '30:00';
+            timeBlack.innerHTML = '35:00';
             break;
     }
 }
 
 
+//display time
 const displayTime = (timeLeft, timeDis) => {
-    // const hours = Math.floor(time/60)
+    //const hours = Math.floor(timeLeft/60)
     const min = Math.floor(timeLeft / 60)
     const sec = Math.floor(timeLeft % 60)
     timeDis.innerHTML = `${min < 10 ? '0' : ''}${min}:${sec < 10 ? '0' : ''}${sec}`
 }
 
 
-// Timer whites
+// Timer, which is not working now
+
+// const timer = (tmCount, tmDisplayed) => {
+//     tmCount--;
+//     displayTime(tmCount, tmDisplayed);
+//     console.log(tmCount)
+// }
+
+//Timer Whites
 
 const timerWhite = () => {
     timeSetWhite--;
-    displayTime(timeSetWhite, timeWhite)
+    displayTime(timeSetWhite, timeWhite);
 }
 
-// Timer blacks
+// Timer Black
 
 const timerBlack = () => {
     timeSetBlack--;
-    displayTime(timeSetBlack, timeBlack)
+    displayTime(timeSetBlack, timeBlack);
 }
 
-// buttons 
-
-let count = false;
-let intervalW = null;
-
-let intervalB = null;
-
-//
+// buttons startStop
 
 plwBtn.addEventListener('click', () => {
     if (count === false) {
@@ -92,3 +112,8 @@ plbBtn.addEventListener('click', () => {
 })
 
 
+// reset button
+
+resetBtn.addEventListener('click', () => {
+    setGameType(gameType)
+})
